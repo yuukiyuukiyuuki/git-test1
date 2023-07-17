6420f38e374eca8e4ab8be47cb373f5300b9a6db
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2023-07-17 03:11:01 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-devise_for :ccustomers,skip: [:passwords], controllers: {
+devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
@@ -7,5 +7,13 @@ devise_for :ccustomers,skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+  # 顧客
+  scope module: :public do
+      root to: 'homes#top'
+  end
+
+  # 管理者
+  namespace :admin do
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
