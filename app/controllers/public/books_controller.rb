33dbe079d03effb32b5a2f944d889f94defb4b79
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2023-07-26 14:07:36 +0000

    コントローラの追加

---
+++ app/controllers/public/books_controller.rb
@@ -0,0 +1,2 @@
+class Public::BooksController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,18 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   # 顧客
   scope module: :public do
       root to: 'homes#top'
+      get '/about' => 'homes#about'
+      #検索
+      get "search" => "searches#search"
+
+      resources :customers
   end
 
   # 管理者
   namespace :admin do
+    #検索
+    get "search" => "searches#search"
+
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
